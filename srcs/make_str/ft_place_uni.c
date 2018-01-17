603f519f5c9c309a246a17f5b8de004ce627e97e
Roman Kyslyy <rkyslyy@Romans-MacBook-Pro.local>
2018-01-17 18:22:44 +0200

    chr

--- srcs/make_str/ft_place_uni.c
+++ srcs/make_str/ft_place_uni.c
@@ -7,7 +7,10 @@ void	ft_place_uni(wchar_t *str)
 	n = 0;
 	while (str[n] != '\0')
 	{
-		ft_print_uni(str[n]);
+		if (str[n] > 127)
+			ft_print_uni(str[n]);
+		else
+			write(1, &str[n], 1);
 		n++;
 	}
 }
